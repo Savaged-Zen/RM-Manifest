42129f0d6125552264081bcf88975507b819f5f6
DrMacinyasha <drmacinyasha@gmail.com>
2011-03-12 12:59:33 -0800

    get dem ROMs

---
+++ manifest.js
@@ -0,0 +1,41 @@
+{
+    version: 1,
+    homepage: "http://www.savaged-zen.org/",
+    donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SHYT7HAG2H2GC",
+    roms:
+    [
+    {
+        name: "Savaged-Zen(MOD) Alpha 1",
+        summary: "Alpha build 001",
+        device: "supersonic",
+        product: "Savaged-Zen(MOD)",
+        incremental: 0001,
+	modversion: "SavagedZen(MOD)-Evo4G-Alpha1",
+        screenshots:
+        [
+	"http://szr.goo-inside.me/screenshots/CAP201103121448.jpg"
+        ],
+	urls:
+        [
+	"http://szr.goo-inside.me/evo/alphas/Savaged-Zen(MOD)-Alpha1.zip"
+        ]
+    },
+    // And here's another, older ROM!
+    {
+        name: "Savaged-Zen(MOD) Alpha 2",
+        summary: "Alpha build 002",
+        device: "supersonic",
+        product: "Savaged-Zen(MOD)",
+        incremental: 0002,
+        modversion: "SavagedZen(MOD)-Evo4G-Alpha2",
+        screenshots:
+        [
+        "http://szr.goo-inside.me/screenshots/CAP201103121448.jpg"
+        ],
+        urls:
+        [
+        "http://szr.goo-inside.me/evo/alphas/Savaged-Zen(MOD)-Alpha2.zip"
+        ]
+    }
+    ]
+}
