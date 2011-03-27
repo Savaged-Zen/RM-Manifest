65b9bd0b71c719d4396830cbd6ec378962873e54
Denham Crafton <drmacinyasha@gmail.com>
2011-03-27 16:40:17 -0700

    Fixes and updates

--- manifest.js
+++ manifest.js
@@ -1,56 +1,53 @@
 {
-    version: 1,
-    homepage: "http://www.savaged-zen.org/",
-    donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SHYT7HAG2H2GC",
-    roms:
+    "version": "1",
+    "homepage": "http://www.savaged-zen.org/",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SHYT7HAG2H2GC",
+    "roms":
     [
     {
-        name: "Savaged-Zen(MOD) Alpha 1",
-        summary: "Alpha build 001",
-        device: "supersonic",
-        product: "Savaged-Zen(MOD)",
-        incremental: 0001,
-	modversion: "SavagedZen(MOD)-Evo4G-Alpha1",
-        screenshots:
-        [
-	"http://szr.goo-inside.me/screenshots/CAP201103121448.jpg"
-        ],
-	urls:
-        [
-	"http://szr.goo-inside.me/evo/alphas/Savaged-Zen(MOD)Alpha1.zip"
-        ]
+        "name": "Savaged-Zen(MOD) Alpha 1",
+        "summary": "Alpha build 001",
+        "device": "supersonic",
+        "product": "Savaged-Zen(MOD)",
+        "incremental": "0001",
+        "modversion": "SavagedZen(MOD)-Evo4G-Alpha1",
+        "screenshots": "http://szr.goo-inside.me/screenshots/CAP201103121448.jpg",
+        "urls": "http://szr.goo-inside.me/evo/alphas/Savaged-Zen(MOD)Alpha1.zip",
+        "addons":
+        {
+            "name": "Google Apps",
+            "urls": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"
+        }
     },
     {
-        name: "Savaged-Zen(MOD) Alpha 2",
-        summary: "Alpha build 002",
-        device: "supersonic",
-        product: "Savaged-Zen(MOD)",
-        incremental: 0002,
-        modversion: "SavagedZen(MOD)-Evo4G-Alpha2",
-        screenshots:
-        [
-        "http://szr.goo-inside.me/screenshots/CAP201103121448.jpg"
-        ],
-        urls:
-        [
-        "http://szr.goo-inside.me/evo/alphas/Savaged-Zen(MOD)Alpha2.zip"
-        ]
-    }
-	    {
-        name: "Savaged-Zen(MOD) RC1",
-        summary: "RC1",
-        device: "supersonic",
-        product: "Savaged-Zen(MOD)",
-        incremental: 0003,
-        modversion: "SavagedZen(MOD)-Evo4G-RC1",
-        screenshots:
-        [
-        "http://szr.goo-inside.me/screenshots/CAP201103121448.jpg"
-        ],
-        urls:
-        [
-        "http://mirror.munkibizness.com/files/rom/SavagedZen(MOD)-RC1-Evo4G.zip"
-        ]
+        "name": "Savaged-Zen(MOD) Alpha 2",
+        "summary": "Alpha build 002",
+        "device": "supersonic",
+        "product": "Savaged-Zen(MOD)",
+        "incremental": "0002",
+        "modversion": "SavagedZen(MOD)-Evo4G-Alpha2",
+        "screenshots": "http://szr.goo-inside.me/screenshots/CAP201103121448.jpg",
+        "urls": "http://szr.goo-inside.me/evo/alphas/Savaged-Zen(MOD)Alpha2.zip",
+        "addons":
+        {
+            "name": "Google Apps",
+            "urls": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"
+        }
+    },
+    {
+        "name": "Savaged-Zen(MOD) RC1",
+        "summary": "RC1",
+        "device": "supersonic",
+        "product": "Savaged-Zen(MOD)",
+        "incremental": "0003",
+        "modversion": "SavagedZen(MOD)-Evo4G-RC1",
+        "screenshots": "http://szr.goo-inside.me/screenshots/CAP201103121448.jpg",
+        "urls": "http://mirror.munkibizness.com/files/rom/SavagedZen(MOD)-RC1-Evo4G.zip",
+        "addons":
+        {
+            "name": "Google Apps",
+            "urls": "http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"
+        }
     }
     ]
 }
